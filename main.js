17d907389d64687c3c8699805dd8aa302115df8c
hugonewton <hugovlnv@gmail.com>
2023-10-26 17:07:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -62,44 +62,95 @@ console.log("OKAYYY")
 
 
 
-var min_x = 0;
-var max_x = 200;
+var maxSearchIterations = 10;
+var min_x = 100;
+var max_x = 500;
 var min_y = 0;
 var max_y = 200;
-var filled_areas = new Array();
+var filled_areas = [];
 
-$('.people-wraper').each(function() {
-    var rand_x=0;
-    var rand_y=0;
-    var area;
-    do {
-        rand_x = Math.round(min_x + ((max_x - min_x)*(Math.random() % 1)));
-        rand_y = Math.round(min_y + ((max_y - min_y)*(Math.random() % 1)));
-        area = {x: rand_x, y: rand_y, width: $(this).width(), height: $(this).height()};
-    } while(check_overlap(area));
-    
-    filled_areas.push(area);
-    
-    $(this).css({left:rand_x, top: rand_y});
-});
+function calc_overlap(a1) {
+    var overlap = 0;
+    for (i = 0; i < filled_areas.length; i++) {
 
-function check_overlap(area) {
-    for (var i = 0; i < filled_areas.length; i++) {
-        
-        check_area = filled_areas[i];
-        
-        var bottom1 = area.y + area.height;
-        var bottom2 = check_area.y + check_area.height;
-        var top1 = area.y;
-        var top2 = check_area.y;
-        var left1 = area.x;
-        var left2 = check_area.x;
-        var right1 = area.x + area.width;
-        var right2 = check_area.x + check_area.width;
-        if (bottom1 < top2 || top1 > bottom2 || right1 < left2 || left1 > right2) {
+        var a2 = filled_areas[i];
+
+        // no intersection cases
+        if (a1.x + a1.width < a2.x) {
             continue;
         }
-        return true;
+        if (a2.x + a2.width < a1.x) {
+            continue;
+        }
+        if (a1.y + a1.height < a2.y) {
+            continue;
+        }
+        if (a2.y + a2.height < a1.y) {
+            continue;
+        }
+
+        // intersection exists : calculate it !
+        var x1 = Math.max(a1.x, a2.x);
+        var y1 = Math.max(a1.y, a2.y);
+        var x2 = Math.min(a1.x + a1.width, a2.x + a2.width);
+        var y2 = Math.min(a1.y + a1.height, a2.y + a2.height);
+
+        var intersection = ((x1 - x2) * (y1 - y2));
+
+        overlap += intersection;
+
+        // console.log("( "+x1+" - "+x2+" ) * ( "+y1+" - "+y2+" ) = " + intersection);
     }
+
+    // console.log("overlap = " + overlap + " on " + filled_areas.length + " filled areas ");
+    return overlap;
+}
+
+function randomize() {
+
+    filled_areas.splice(0, filled_areas.length);
+
+    var index = 0;
+    $('.people-wrapper').each(function() {
+        var rand_x = 0;
+        var rand_y = 0;
+        var i = 0;
+        var smallest_overlap = 9007199254740992;
+        var best_choice;
+        var area;
+        for (i = 0; i < maxSearchIterations; i++) {
+            rand_x = Math.round(min_x + ((max_x - min_x) * (Math.random() % 1)));
+            rand_y = Math.round(min_y + ((max_y - min_y) * (Math.random() % 1)));
+            area = {
+                x: rand_x,
+                y: rand_y,
+                width: $(this).width(),
+                height: $(this).height()
+            };
+            var overlap = calc_overlap(area);
+            if (overlap < smallest_overlap) {
+                smallest_overlap = overlap;
+                best_choice = area;
+            }
+            if (overlap === 0) {
+                break;
+            }
+        }
+
+        filled_areas.push(best_choice);
+
+        $(this).css({
+            position: "absolute",
+            "z-index": index++
+        });
+        $(this).animate({
+            left: rand_x,
+            top: rand_y
+        });
+
+        // console.log("and the winner is : " + smallest_overlap);
+    });
     return false;
 }
+
+randomize();
