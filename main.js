b0822bb903d0420a10a65d4e4296deeafad31e65
hugonewton <hugovlnv@gmail.com>
2023-10-26 16:24:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ console.log("OKAYYY")
     "use strict";
     const TRIES_PER_BOX = 50;
     const randUint = range => Math.random() * range | 0;
-    const placing  = [...document.querySelectorAll(".poeple-wrapper")].map(el => Bounds(el, 5));
+    const placing  = [...document.querySelectorAll(".people-wrapper")].map(el => Bounds(el, 5));
     const fitted = [];
     const areaToFit = Bounds();
     var maxTries = TRIES_PER_BOX * placing.length;
